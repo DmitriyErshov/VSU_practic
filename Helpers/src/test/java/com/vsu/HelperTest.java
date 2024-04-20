49f07d9aa51fc71a46b68b437545aac222d4928f
Дмитрий <dmitriy-ershov-01@ya.ru>
2024-04-20 21:07:13 +0300

    added tests

---
+++ Helpers/src/test/java/com/vsu/HelperTest.java
@@ -0,0 +1,19 @@
+package com.vsu;
+
+import com.vsu.HashCollection.HashTable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class HelperTest {
+    @BeforeEach
+    void load() throws Exception {
+
+    }
+    @Test
+    void addElem() {
+
+    }
+}
